1cb03b5cb66eb4d0fb708fc7c13e23ab04f30c78
Kurkov11 <kurkovsw@icloud.com>
2023-12-29 16:13:17 +0100

    Setup notification counting in js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 let notificationArray = document.getElementsByClassName("notification");
+let nCount = 0;
+for(let i = 0; i < notificationArray.length; i++){
+    if(notificationArray[i].style.backgroundColor = 'hsl(210, 60%, 98%)'){
+        nCount++;
+    }
+}
+document.getElementById('notification-count').innerHTML = nCount;
 document.getElementById('mark-as-read').onclick = function(){
     for(let i = 0; i < notificationArray.length; i++){
         notificationArray[i].style.backgroundColor = 'white';
     }
+    nCount = 0;
+    document.getElementById('notification-count').innerHTML = nCount;
 }
 for(let i = 0; i < notificationArray.length; i++){
     notificationArray[i].onclick = function(){
         notificationArray[i].style.backgroundColor = 'white';
+        nCount--;
+        document.getElementById('notification-count').innerHTML = nCount;
     }
 }
