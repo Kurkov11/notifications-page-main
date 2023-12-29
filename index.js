d50329ceadcb30fa028116ccf8547fc0da22cd2b
Kurkov11 <kurkovsw@icloud.com>
2023-12-29 16:16:43 +0100

    Fix: change notification count only when the background color of clicked element is not white

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ for(let i = 0; i < notificationArray.length; i++){
 document.getElementById('notification-count').innerHTML = nCount;
 document.getElementById('mark-as-read').onclick = function(){
     for(let i = 0; i < notificationArray.length; i++){
-        notificationArray[i].style.backgroundColor = 'white';
+        if(notificationArray[i].style.backgroundColor != 'white'){
+            notificationArray[i].style.backgroundColor = 'white';
+        }
     }
     nCount = 0;
     document.getElementById('notification-count').innerHTML = nCount;
 }
 for(let i = 0; i < notificationArray.length; i++){
     notificationArray[i].onclick = function(){
-        notificationArray[i].style.backgroundColor = 'white';
-        nCount--;
-        document.getElementById('notification-count').innerHTML = nCount;
+        if(notificationArray[i].style.backgroundColor != 'white'){
+            notificationArray[i].style.backgroundColor = 'white';
+            nCount--;
+            document.getElementById('notification-count').innerHTML = nCount;
+        }
     }
 }
