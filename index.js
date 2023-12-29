e0d5d9b11d310fdbeaa258d110551a7c9cc2e6e4
Kurkov11 <kurkovsw@icloud.com>
2023-12-29 16:53:11 +0100

    Make the dots disappear onclick if the background is not white

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let notificationArray = document.getElementsByClassName("notification");
 let nCount = 0;
+let dotArray = document.getElementsByClassName('dot');
 for(let i = 0; i < notificationArray.length; i++){
     if(notificationArray[i].style.backgroundColor = 'hsl(210, 60%, 98%)'){
         nCount++;
@@ -10,6 +11,7 @@ document.getElementById('mark-as-read').onclick = function(){
     for(let i = 0; i < notificationArray.length; i++){
         if(notificationArray[i].style.backgroundColor != 'white'){
             notificationArray[i].style.backgroundColor = 'white';
+            dotArray[i].style.opacity = '0';
         }
     }
     nCount = 0;
@@ -21,6 +23,7 @@ for(let i = 0; i < notificationArray.length; i++){
             notificationArray[i].style.backgroundColor = 'white';
             nCount--;
             document.getElementById('notification-count').innerHTML = nCount;
+            dotArray[i].style.opacity = '0';
         }
     }
 }
